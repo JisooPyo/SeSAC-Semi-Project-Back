16ab077c3e976fb2ea293a4b93afdbae3d87399f
JisooPyo <audwl326@gmail.com>
2024-09-10 16:00:18 +0900

    feat: Validation 예외 Handler 구현

--- src/main/java/sesac/semiProject/common/exception/GlobalExceptionHandler.java
+++ src/main/java/sesac/semiProject/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,9 @@
 package sesac.semiProject.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +17,16 @@ public class GlobalExceptionHandler {
         ApiResponseDto apiResponseDto = new ApiResponseDto(
             e.getErrorCode().getStatusCode(), e.getMessage()
         );
+        return ResponseEntity.badRequest().body(apiResponseDto);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<ApiResponseDto> handlerValicationException(MethodArgumentNotValidException ex) {
+        ApiResponseDto apiResponseDto = null;
+        for (FieldError fieldError : ex.getFieldErrors()) {
+            apiResponseDto = new ApiResponseDto(HttpStatus.BAD_REQUEST.value(), fieldError.getDefaultMessage());
+            break;
+        }
         return ResponseEntity.badRequest().body(apiResponseDto);
     }
 }
