335c5b366b8bb122823c7b5c9c64cb40295db707
JisooPyo <audwl326@gmail.com>
2024-09-10 16:37:05 +0900

    feat: 할 일 목록 조회 API 구현

--- src/main/java/sesac/semiProject/todo/controller/TodoController.java
+++ src/main/java/sesac/semiProject/todo/controller/TodoController.java
@@ -1,6 +1,9 @@
 package sesac.semiProject.todo.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +24,10 @@ public class TodoController {
     @PostMapping("/todos")
     public ResponseEntity<TodoResponseDto> createTodo(@RequestBody @Valid TodoRequestDto requestDto) {
         return ResponseEntity.ok().body(todoService.createTodo(requestDto));
+    }
+
+    @GetMapping("/todos")
+    public ResponseEntity<List<TodoResponseDto>> getAllTodos() {
+        return ResponseEntity.ok().body(todoService.getAllTodos());
     }
 }
