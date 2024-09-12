7213e7922f6dfb67ee2d8349ccca940b20091a5f
JisooPyo <audwl326@gmail.com>
2024-09-12 11:55:52 +0900

    chore: Member - Todo OneToMany 연결

--- src/main/java/sesac/semiProject/member/model/Member.java
+++ src/main/java/sesac/semiProject/member/model/Member.java
@@ -1,14 +1,20 @@
 package sesac.semiProject.member.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import sesac.semiProject.todo.model.Todo;
 
 @Entity
 @Getter
@@ -28,4 +34,8 @@ public class Member {
 
     @Column(length = 50)
     private String nickname;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    @Builder.Default
+    private List<Todo> todoList = new ArrayList<>();
 }
