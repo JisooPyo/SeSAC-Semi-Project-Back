0d535f867994caae602e47aa79eed70b27a493b7
JisooPyo <audwl326@gmail.com>
2024-09-12 11:48:44 +0900

    init: Member Model 구현

---
+++ src/main/java/sesac/semiProject/member/model/Member.java
@@ -0,0 +1,31 @@
+package sesac.semiProject.member.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(length = 50)
+    private String nickname;
+}
