8ce67e8a21b9bf4d2e8c1b3e7e2e62f8b0165cdb
JisooPyo <audwl326@gmail.com>
2024-09-15 19:46:47 +0900

    [Feature] 멤버 닉네임 불러오기 (#31)
    * feat: 멤버 닉네임 불러오기 구현
    
    MemberController - 공통 endpoint api/members로 변경
    
    * test: 멤버 닉네임 불러오기 구현 테스트

--- src/main/java/sesac/semiProject/member/controller/MemberController.java
+++ src/main/java/sesac/semiProject/member/controller/MemberController.java
@@ -1,6 +1,8 @@
 package sesac.semiProject.member.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +12,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import sesac.semiProject.common.dto.ApiResponseDto;
+import sesac.semiProject.common.security.UserDetailsImpl;
 import sesac.semiProject.member.dto.LoginRequestDto;
 import sesac.semiProject.member.dto.SignupRequestDto;
 import sesac.semiProject.member.service.MemberService;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/members")
 public class MemberController {
     private final MemberService memberService;
 
@@ -30,5 +33,10 @@ public class MemberController {
         @RequestBody LoginRequestDto requestDto,
         HttpServletResponse response) {
         return ResponseEntity.ok().body(memberService.login(requestDto, response));
+    }
+
+    @GetMapping("/nickname")
+    public ResponseEntity<String> getNickname(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseEntity.ok().body(userDetails.getMember().getNickname());
     }
 }
