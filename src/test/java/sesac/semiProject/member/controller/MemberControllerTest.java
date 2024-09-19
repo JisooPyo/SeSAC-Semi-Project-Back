f30dd6f6896e8b6beebe3e809b8849bffa29cf12
JisooPyo <audwl326@gmail.com>
2024-09-19 10:08:43 +0900

    test: MemberController - 회원가입 API 테스트 코드 작성

--- src/test/java/sesac/semiProject/member/controller/MemberControllerTest.java
+++ src/test/java/sesac/semiProject/member/controller/MemberControllerTest.java
@@ -19,6 +19,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -28,6 +29,7 @@ import sesac.semiProject.common.security.SecurityConfig;
 import sesac.semiProject.common.security.UserDetailsImpl;
 import sesac.semiProject.common.security.UserDetailsServiceImpl;
 import sesac.semiProject.member.dto.LoginRequestDto;
+import sesac.semiProject.member.dto.SignupRequestDto;
 import sesac.semiProject.member.model.Member;
 import sesac.semiProject.member.service.MemberService;
 
@@ -50,10 +52,13 @@ class MemberControllerTest {
     @MockBean
     private MemberService memberService;
 
+    ObjectMapper objectMapper;
+
     String nickname = "testUser";
 
     @BeforeEach
     void setUp() {
+        objectMapper = new ObjectMapper();
         userDetails = Mockito.mock(UserDetailsImpl.class);
         SecurityContextHolder.getContext().setAuthentication(
             new UsernamePasswordAuthenticationToken(userDetails, null, null)
@@ -61,6 +66,7 @@ class MemberControllerTest {
     }
 
     @Test
+    @DisplayName("멤버 닉네임 가져오기: 성공")
     @WithMockUser
     void getNickname() throws Exception {
         // given
@@ -74,6 +80,7 @@ class MemberControllerTest {
     }
 
     @Test
+    @DisplayName("로그인: 성공")
     void login() throws Exception {
         // given
         LoginRequestDto loginRequestDto =
@@ -90,9 +97,68 @@ class MemberControllerTest {
         // when
         mockMvc.perform(post("/api/members/login")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(loginRequestDto)))
+                .content(objectMapper.writeValueAsString(loginRequestDto)))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.statusCode").value(expectedStatusCode))
             .andExpect(jsonPath("$.message").value(expectedMessage));
+    }
+
+    @Test
+    @DisplayName("회원가입: 성공")
+    void signup() throws Exception {
+        // given
+        SignupRequestDto signupRequestDto =
+            SignupRequestDto.builder()
+                .email("test-user@email.com")
+                .password("password123!")
+                .nickname("test-nickname")
+                .build();
+        int expectedStatusCode = HttpStatus.OK.value();
+        String expectedMessage = "test-message";
+        ApiResponseDto responseDto = new ApiResponseDto(expectedStatusCode, expectedMessage);
+        given(memberService.signup(any(SignupRequestDto.class))).willReturn(responseDto);
+
+        // when
+        mockMvc.perform(post("/api/members/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signupRequestDto)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.statusCode").value(expectedStatusCode))
+            .andExpect(jsonPath("$.message").value(expectedMessage));
+    }
+
+    // todo
+    @Test
+    @DisplayName("회원가입: 실패 - 이메일 형식이 올바르지 않을 때")
+    void signupFailureByInvalidEmail() {
+
+    }
+
+    // todo
+    @Test
+    @DisplayName("회원가입: 실패 - 비밀번호가 8자 미만일 때")
+    void signupFailureByTooShortPassword() {
+
+    }
+
+    // todo
+    @Test
+    @DisplayName("회원가입: 실패 - 비밀번호 규칙(영소문자, 숫자, 특수문자 1개 이상 포함)에 맞지 않을 때")
+    void signupFailureByInvalidPassword() {
+
+    }
+
+    // todo
+    @Test
+    @DisplayName("회원가입: 실패 - 닉네임이 빈 칸으로 들어왔을 때")
+    void signupFailureByEmptyNickname() {
+
+    }
+
+    // todo
+    @Test
+    @DisplayName("회원가입: 실패 - 닉네임이 null 로 들어올 때")
+    void signupFailureByNullNickname() {
+
     }
 }
